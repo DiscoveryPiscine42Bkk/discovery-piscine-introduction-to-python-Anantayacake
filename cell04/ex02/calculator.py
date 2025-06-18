eb13efe7a79f31fb94d94323287ec8e839041a5e
Anantayacake <cake132313@gmail.com>
2025-06-18 05:41:50 +0000

    python

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+first = float(input("Give me the first number: "))
+second = float(input("Give me the second number: "))
+print("Thank you!")
+print(f"{first} + {second} = {first + second}")
+print(f"{first} - {second} = {first - second}")
+print(f"{first} / {second} = {first / second}")
+print(f"{first} * {second} = {first * second}")
