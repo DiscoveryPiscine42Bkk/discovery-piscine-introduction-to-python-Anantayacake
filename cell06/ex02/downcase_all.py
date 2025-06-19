733ef8fa5af7a6956d75ff36b811d5906aabf89f
Anantayacake <cake132313@gmail.com>
2025-06-19 07:15:46 +0000

    python

--- cell06/ex02/downcase_all.py
+++
@@ -1,8 +0,0 @@
-import sys
-def downcase_it(s):
-    return s.lower()
-if len(sys.argv) <= 1:
-    print("none")
-else:
-    for arg in sys.argv[1:]:
-        print(downcase_it(arg))
--- cell06/ex05/scope_that.py
+++
@@ -1,7 +0,0 @@
-def add_one(x):
-    x = x + 1
-    print("Inside add_one, x =", x)
-num = 5
-print("Before add_one, num =", num)
-add_one(num)
-print("After add_one, num =", num)
